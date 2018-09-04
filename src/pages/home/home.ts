86649304ef8ba29573f2310c0c17a6017182e44f
vithort <vithort@gmail.com>
2018-09-04 11:24:39 -0300

    List View & Parse JSON
    Incluindo o List View e Parse JSON via HTTP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { Observable } from '../../../node_modules/rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public items: any;
 
+  constructor(
+    public navCtrl: NavController
+    ,public http: HttpClient
+  ) {
+    this.loadData();
+  }
+
+  loadData() {
+    let data: Observable<any>;
+    data = this.http.get('https://jsonplaceholder.typicode.com/posts');
+    data.subscribe(results => {
+      this.items = results;
+    })
+  }
+
+  itemClick(itemid:number) {
+    alert(itemid);
   }
 
 }
